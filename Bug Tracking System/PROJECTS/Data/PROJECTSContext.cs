d2ff64f1fd21358eb54459cc9ff51c7f13005bf5
nandini-27374 <gurikaninandini26@gmail.com>
2025-06-12 23:22:52 +0530

    Bug Tracking System

---
+++ Bug Tracking System/PROJECTS/Data/PROJECTSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PROJECTS.Models;
+
+namespace PROJECTS.Data
+{
+    public class PROJECTSContext : DbContext
+    {
+        public PROJECTSContext (DbContextOptions<PROJECTSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PROJECTS.Models.Bug> Bug { get; set; } = default!;
+    }
+}
---
+++ Bug Tracking System/PROJECTS/Models/Bug.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROJECTS.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; } // Primary key
+        [Required, StringLength(100)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string Severity { get; set; } // "Low", "Medium", "High"
+        [Required]
+        public string Status { get; set; } // "Open", "In Progress", "Closed"
+        public string Assignee { get; set; }
+        public DateTime ReportedDate { get; set; }
+    }
+}
